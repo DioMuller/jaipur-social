39fc1d836c9cf553149f6c54706eaa1689b27d65
Diogo Muller <diogo.muller@live.com>
2013-08-23 10:19:24 -0300

    INT: Added database persistency.

--- src/JaipurSocial/Default.aspx.cs
+++ src/JaipurSocial/Default.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JaipurSocial.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,10 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        
+        //using (var db = new JaipurEntities())
+        //{
+        //    db.User.Add(new User() { Email = "diogo.muller@live.com", Password = new byte[256] });
+        //    db.SaveChanges();
+        //}
     }
 }
