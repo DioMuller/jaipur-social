b6cdcd1b2b42c5b6c8f3d750b8f80c9f063cd28a
Joao Vitor P. Moraes <jvlppm@gmail.com>
2013-09-26 23:07:04 -0300

    INT: Added missing file.

---
+++ src/JaipurSocial/App_Code/RunningGameInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for RunninGameInfo
+/// </summary>
+public class RunningGameInfo
+{
+    public int GameId { get; set; }
+    public string EnemyLogin { get; set; }
+    public string EnemyEmail { get; set; }
+    public string GameStatus { get; set; }
+}
